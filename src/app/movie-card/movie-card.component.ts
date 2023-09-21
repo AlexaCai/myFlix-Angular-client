452a5613bc6e2dfca2dae27096af0758271ce265
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-09-21 17:24:22 -0600

    Addition of the logic to display the Genre details

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
+import { MatDialog } from '@angular/material/dialog';
+import { GenreDetailsComponent } from '../genre-details/genre-details.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -8,17 +10,30 @@ import { FetchApiDataService } from '../fetch-api-data.service'
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog
+  ) { }
 
-ngOnInit(): void {
-  this.getMovies();
-}
+  ngOnInit(): void {
+    this.getMovies();
+  }
 
-getMovies(): void {
-  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
     });
   }
+
+  openGenreDialogue(genre: any): void {
+    this.dialog.open(GenreDetailsComponent, {
+      data: {
+        Name: genre.Name,
+        Description: genre.Description,
+      }
+    });
+  }
+
 }
