bd5cafb6349252a9b096b0f8ff5de6e68f404038
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-09-21 12:29:21 -0600

    Update on the visual display for the Movie cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 const appRoutes: Routes = [
@@ -45,6 +46,7 @@ const appRoutes: Routes = [
     MatCardModule,
     MatFormFieldModule,
     MatSnackBarModule,
+    MatIconModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
