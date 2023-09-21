7c49e0fdebaec65bd7beb8e14c0df4e9e0d8d1a0
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-09-21 11:40:15 -0600

    Addition of MovieCard component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,20 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
 
   constructor(public dialog: MatDialog) { }
   // This is the function that will open the dialog when the signup button is clicked  
@@ -20,10 +25,20 @@ export class AppComponent {
       width: '280px'
     });
   }
+
+
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       // Assigning the dialog a width
       width: '280px'
+    });
+  }
+
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px',
+      height: '600px'
     });
   }
   
